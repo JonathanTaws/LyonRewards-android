4eb35dc245b3bf0bfd0f36a2cb8e73d8a02a1929
Pierre Jarsaillon <pierre.jarsaillon@insa-lyon.fr>
2016-04-26 11:08:45 +0200

    Init project

---
+++ LyonRewards/app/src/main/java/h4311/hexanome/insa/lyonrewards/MainActivity.java
@@ -0,0 +1,13 @@
+package h4311.hexanome.insa.lyonrewards;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
