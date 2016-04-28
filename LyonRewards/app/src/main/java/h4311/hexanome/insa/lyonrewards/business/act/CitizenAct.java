7ca482bde469904bae4f088f182b57615efe1c3a
Pierre Jarsaillon <pierre.jarsaillon@insa-lyon.fr>
2016-04-28 15:10:20 +0200

    Add QRCodeCitizenAct business object

--- LyonRewards/app/src/main/java/h4311/hexanome/insa/lyonrewards/business/act/CitizenAct.java
+++ LyonRewards/app/src/main/java/h4311/hexanome/insa/lyonrewards/business/act/CitizenAct.java
@@ -1,17 +1,99 @@
 package h4311.hexanome.insa.lyonrewards.business.act;
 
-/**
- * Created by Jonathan on 28/04/2016.
- */
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
 public class CitizenAct {
 
-    private int id;
+    @SerializedName("id")
+    @Expose
+    protected Integer id;
 
-    public int getId() {
+    @SerializedName("title")
+    @Expose
+    protected String title;
+
+    @SerializedName("description")
+    @Expose
+    protected String description;
+
+    @SerializedName("points")
+    @Expose
+    protected Integer points;
+
+    /**
+     * No args constructor for use in serialization
+     */
+    public CitizenAct() {
+    }
+
+    /**
+     * @param id
+     * @param title
+     * @param description
+     * @param points
+     */
+    public CitizenAct(Integer id, String title, String description, Integer points) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.points = points;
+    }
+
+
+    /**
+     * @return The id
+     */
+    public Integer getId() {
         return id;
     }
 
-    public void setId(int id) {
+    /**
+     * @param id The id
+     */
+    public void setId(Integer id) {
         this.id = id;
+    }
+
+    /**
+     * @return The title
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * @param title The title
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    /**
+     * @return The description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * @param description The description
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     * @return The points
+     */
+    public Integer getPoints() {
+        return points;
+    }
+
+    /**
+     * @param points The points
+     */
+    public void setPoints(Integer points) {
+        this.points = points;
     }
 }
--- LyonRewards/app/src/main/java/h4311/hexanome/insa/lyonrewards/business/act/QRCodeCitizenAct.java
+++ LyonRewards/app/src/main/java/h4311/hexanome/insa/lyonrewards/business/act/QRCodeCitizenAct.java
@@ -1,8 +1,47 @@
 package h4311.hexanome.insa.lyonrewards.business.act;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Created by Jonathan on 28/04/2016.
  */
 public class QRCodeCitizenAct extends CitizenAct {
 
+    @SerializedName("treasure_hunt")
+    @Expose
+    private Integer treasureHunt;
+
+    /**
+     * No args constructor for use in serialization
+     */
+    public QRCodeCitizenAct() {
+    }
+
+    /**
+     * @param id
+     * @param title
+     * @param description
+     * @param treasureHunt
+     * @param points
+     */
+    public QRCodeCitizenAct(Integer id, String title, String description, Integer points, Integer treasureHunt) {
+        super(id, title, description, points);
+        this.treasureHunt = treasureHunt;
+    }
+
+
+    /**
+     * @return The treasureHunt
+     */
+    public Integer getTreasureHunt() {
+        return treasureHunt;
+    }
+
+    /**
+     * @param treasureHunt The treasure_hunt
+     */
+    public void setTreasureHunt(Integer treasureHunt) {
+        this.treasureHunt = treasureHunt;
+    }
 }
