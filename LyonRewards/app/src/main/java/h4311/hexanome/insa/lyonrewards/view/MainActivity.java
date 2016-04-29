b7d881380ca27b6bc4aa57caafab5ac384b718e0
JonathanTaws <jonathan.taws@gmail.com>
2016-04-29 11:32:59 +0200

    Corrected bug with QR scanner fragment selector

--- LyonRewards/app/src/main/java/h4311/hexanome/insa/lyonrewards/view/MainActivity.java
+++ LyonRewards/app/src/main/java/h4311/hexanome/insa/lyonrewards/view/MainActivity.java
@@ -160,7 +160,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         FragmentManager fragmentManager = getSupportFragmentManager();
         fragmentManager.beginTransaction()
-                .replace(R.id.main_activity_content_frame, fragment)
+                .replace(R.id.main_activity_content_frame, fragment, tag)
                 .commit();
 
         if (toolbar != null) {
@@ -197,6 +197,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             }
 
             fragment = QrReaderFragment.newInstance(bundle);
+            fragmentName = MainActivity.QR_SCANNER_FRAGMENT;
         }
         else if(id == R.id.nav_events) {
             fragment = EventsFragment.newInstance(bundle);
