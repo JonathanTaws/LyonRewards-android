facd0d30c4e2c4210fe0216fe32438647bf70fad
JonathanTaws <jonathan.taws@gmail.com>
2016-04-29 18:15:33 +0200

    Added reflection to diminish autofocus interval

---
+++ LyonRewards/app/src/main/java/com/google/zxing/client/android/camera/open/ReflectionUtils.java
@@ -0,0 +1,28 @@
+package com.google.zxing.client.android.camera.open;
+
+import android.util.Log;
+
+import java.lang.reflect.Field;
+
+/**
+ * Created by Jonathan on 29/04/2016.
+ */
+public class ReflectionUtils {
+
+    public static void performInjection() {
+        injectClassField(AutoFocusManager.class, "AUTO_FOCUS_INTERVAL_MS", 2000L);
+    }
+
+    private static void injectClassField(Class clazz, String fieldStr, long newValue) {
+        try {
+            final Field field = clazz.getDeclaredField(fieldStr);
+            if (field != null) {
+                field.setAccessible(true);
+                Object obj = null;
+                field.set(obj, newValue);
+            }
+        } catch (Exception e) {
+            Log.d("REFLECTION", clazz.getSimpleName() + " injection failed for field: " + fieldStr);
+        }
+    }
+}
