e391fe91a0ce5b0ae69826ca91e9d64313968bad
Pierre Jarsaillon <pierre.jarsaillon@insa-lyon.fr>
2016-05-01 13:47:11 +0200

    Update notification's icon

--- LyonRewards/app/src/main/java/h4311/hexanome/insa/lyonrewards/di/module/gcm/MyGcmListenerService.java
+++ LyonRewards/app/src/main/java/h4311/hexanome/insa/lyonrewards/di/module/gcm/MyGcmListenerService.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2015 Google Inc. All Rights Reserved.
- *
+ * <p/>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * <p/>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p/>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,16 +16,20 @@
 
 package h4311.hexanome.insa.lyonrewards.di.module.gcm;
 
+import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
+import android.view.View;
 
 import com.google.android.gms.gcm.GcmListenerService;
 
@@ -35,6 +39,8 @@ import h4311.hexanome.insa.lyonrewards.view.MainActivity;
 public class MyGcmListenerService extends GcmListenerService {
 
     private static final String TAG = "MyGcmListenerService";
+
+    private static final String DATA_ID_OFFER = "id_offer";
 
     /**
      * Called when message is received.
@@ -72,52 +78,62 @@ public class MyGcmListenerService extends GcmListenerService {
 
         Intent registrationComplete = new Intent(QuickstartPreferences.GCM_MESSAGE_RECEIVED);
 // todo : set constant
-        registrationComplete.putExtra(MainActivity.ARG_ID_OFFER_READ, data.getString("id_offer"));
+        registrationComplete.putExtra(MainActivity.ARG_ID_OFFER_READ, data.getString(DATA_ID_OFFER));
         LocalBroadcastManager.getInstance(this).sendBroadcast(registrationComplete);
 
         /**
          * In some cases it may be useful to show a notification indicating to the user
          * that a message was received.
          */
-        sendNotification(data.getString("id_offer"), data.getString("score"), data.getString("title"));
+        sendNotification(data.getString(DATA_ID_OFFER), data.getString("score"), data.getString("title"));
         // [END_EXCLUDE]
     }
     // [END receive_message]
 
     /**
      * Create and show a simple notification containing the received GCM message.
-     *
      */
     private void sendNotification(String idOffer, String points, String title) {
-        // todo : redirect to offer fragment
-
         Intent intent = new Intent(this, MainActivity.class);
-       // intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
-
-        //intent.putExtra(MainActivity.ARG_ID_OFFER_READ, idOffer);
-        //intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
         intent.putExtra(MainActivity.ARG_ID_OFFER_READ, idOffer);
         intent.setAction("TESTACT");
-        //intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP
-          //      | Intent.FLAG_ACTIVITY_SINGLE_TOP );
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
                 PendingIntent.FLAG_UPDATE_CURRENT);
 
 
+        Bitmap icon = BitmapFactory.decodeResource(getResources(), R.mipmap.lyon_rewards_logo);
+
 
         Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
-                .setSmallIcon(R.drawable.icon_success_key_finished)
+                .setSmallIcon(R.mipmap.lyon_rewards_logo)
+                .setLargeIcon(icon)
                 .setContentTitle("Offre partenaire utilisée")
                 .setContentText(title + " (- " + points + " points).")
                 .setAutoCancel(true)
                 .setSound(defaultSoundUri)
-                .setContentIntent(pendingIntent);
+                .setContentIntent(pendingIntent)
+                .extend(new NotificationCompat.WearableExtender().setBackground(icon));
 
         NotificationManager notificationManager =
                 (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
+        Notification notif = notificationBuilder.build();
+
+        // Hide the small icon in the right bottom corner
+        int smallIconViewId = getResources().getIdentifier("right_icon", "id", android.R.class.getPackage().getName());
+        if (smallIconViewId != 0) {
+            if (notif.contentIntent != null)
+                notif.contentView.setViewVisibility(smallIconViewId, View.INVISIBLE);
+
+            if (notif.headsUpContentView != null)
+                notif.headsUpContentView.setViewVisibility(smallIconViewId, View.INVISIBLE);
+
+            if (notif.bigContentView != null)
+                notif.bigContentView.setViewVisibility(smallIconViewId, View.INVISIBLE);
+        }
+
         // todo : id notification
-        notificationManager.notify(0, notificationBuilder.build());
+        notificationManager.notify(0, notif);
     }
 }
